73ad2ca0438753487b35ca72dcba772d372c79ae
Bernardo <bernardoalvesbr2025@gmail.com>
2025-08-15 12:32:59 -0300

    feat: added morgan for detailed logs

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
-import authRoutes from "./routes/auth.routes.js";
 import helmet from "helmet";
 import compression from "compression";
+import morgan from "morgan";
+
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ app.use(helmet());
 app.use(cors());
 app.use(compression());
 app.use(express.json());
+
+app.use(morgan("combined"));
 
 app.use("/api/auth", authRoutes);
 
